393f9528dbf1e49d70cbb32c7a9c5c58da30cd85
changlin93 <767425412@qq.com>
2021-07-13 22:15:26 +0800

    feat: [main] 调整格式及集成naive-ui库

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <NButton @click="handleAdd">add</NButton>
+    <NButton @click="handleMinus">minus</NButton>
+    <n-progress type="circle" color="green" :percentage="percentage" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    // name: 'App',
+    // data() {
+    //     return {
+    //         percentage: 0
+    //     }
+    // },
+    // methods: {
+    //     add() {
+    //         this.percentage += 10
+    //         if (this.percentage > 100) this.percentage = 0
+    //     },
+    //     minus() {
+    //         this.percentage -= 10
+    //         if (this.percentage < 0) this.percentage = 100
+    //     }
+    // }
+    setup() {
+        let percentage = ref<number>(0)
+        function handleAdd() {
+            if (percentage.value < 100) percentage.value += 10
+        }
+        function handleMinus() {
+            if (percentage.value > 0) percentage.value -= 10
+        }
+        return {
+            percentage,
+            handleAdd,
+            handleMinus
+        }
+    }
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+// import { NButton } from 'naive-ui'
 import App from './App.vue'
+import naive from './plugins/naive'
 
-createApp(App).mount('#app')
+// 通用字体
+import 'vfonts/Lato.css'
+// 等宽字体
+import 'vfonts/FiraCode.css'
+
+const app = createApp(App)
+app.use(naive)
+app.mount('#app')
